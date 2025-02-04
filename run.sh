122b154bec1a1e2f22c344da841a6dbc6e83c7f2
jboulanger <jerome.boulanger@laposte.net>
2025-02-04 13:36:06 +0000

    update script

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --mail-type=END,FAIL
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=05:00:00
+#SBATCH --partition=agpu
+#SBATCH --gres=gpu:1
+
+echo "TASK ID : $SLURM_ARRAY_TASK_ID"
+
+source=/cephfs2/jparham/
+destination=/media/cephfs2/jeromeb/userdata/Baum_group/jparham/Analysis8
+
+apptainer exec \
+	--writable-tmpfs \
+	--bind /cephfs2:/cephfs2,/cephfs:/cephfs,/lmb:/lmb \
+	/public/singularity/containers/lightmicroscopy/bioimaging-container/bioimaging.sif \
+	/bin/micromamba run -n imaging \
+	python dnasufo.py \
+	-s $source \
+	-d $destination \
+	-n $SLURM_ARRAY_TASK_ID
